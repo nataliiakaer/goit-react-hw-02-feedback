4492cff59e153cd92f655001fad6320ba30b9a43
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-06-06 07:50:43 +0300

    Task 1 add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,23 @@
+import React, { Component, Fragment } from 'react';
+import Feedback from './Feedback';
 
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <Fragment>
+        <Feedback
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+        />
+        {/* <Statistics good={} neutral={} bad={} total={} positivePercentage={}></Statistics> */}
+      </Fragment>
+    );
+  }
+}
